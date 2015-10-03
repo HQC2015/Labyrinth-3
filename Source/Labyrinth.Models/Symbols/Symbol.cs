45fe1f740127d3ad1d513029263d237225ad3b54
kossov <ogi_2112@yahoo.com>
2015-10-03 18:07:56 +0300

    small fix

--- Source/Labyrinth.Models/Symbols/Symbol.cs
+++ Source/Labyrinth.Models/Symbols/Symbol.cs
@@ -4,16 +4,16 @@
 
     public abstract class Symbol : ISymbol
     {
+        private readonly char value;
+
         protected Symbol(char val)
         {
-            this.Value = val;
+            this.value = val;
         }
-
-        private char Value { get; set; }
 
         public char GetValue()
         {
-            return this.Value;
+            return this.value;
         }
     }
 }
