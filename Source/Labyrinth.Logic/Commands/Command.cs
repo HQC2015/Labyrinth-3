790e885f994089326a6a86a51fe96d4c4d488e4e
DJBuro <DJBuro@gmail.com>
2015-10-04 20:17:43 +0300

    Added template for Command pattern

---
+++ Source/Labyrinth.Logic/Commands/Command.cs
@@ -0,0 +1,97 @@
+﻿using Labyrinth.Common;
+using Labyrinth.Logic.Contracts;
+using Labyrinth.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Labyrinth.Logic.Commands
+{
+    public class Command
+    {
+        private MoveLogic moveLogic;
+        private List<string> commands = new List<string>();
+        private int currentCommandIndex;
+        private string command;
+        private IInputHandler inputHandler;
+        private IRenderer renderer;
+
+        public Command()
+        {
+            this.command = inputHandler.GetInput();
+        }
+
+        public void Start()
+        {
+            renderer.RenderMessage(Messages.EnterMoveMessage);
+            switch (this.command.ToLower())
+            {
+                case "top":
+                    this.Top();
+                    break;
+                case "exit":
+                    this.Exit();
+                    break;
+                case "restart":
+                    this.Restart();
+                    break;
+                case "b":
+                    this.Undo();
+                    break;
+                case "f":
+                    this.Redo();
+                    break;
+                default:
+                    this.renderer.RenderMessage("Invalid command!");
+                    break;
+            }
+        }
+
+        public void Restart()
+        {
+
+        }
+
+        public void Exit()
+        {
+
+        }
+
+        public void Top()
+        {
+            this.renderer.RenderScoreboard(Scoreboard.Instance);
+            this.renderer.RenderMessage("\n");
+            this.renderer.RenderBoard(Board.Instance);
+        }
+
+        public void Redo()
+        {
+            if (this.currentCommandIndex < this.commands.Count - 1)
+            {
+                var command = this.commands[this.currentCommandIndex++];
+                CommandExecutor commandForExecution = new CommandExecutor(command, moveLogic);
+                commandForExecution.Execute();
+                this.currentCommandIndex++;
+            }
+        }
+
+        public void Undo()
+        {
+            if (this.currentCommandIndex > 0)
+            {
+                var command = this.commands[--this.currentCommandIndex];
+                CommandExecutor commandForExecution = new CommandExecutor(command, moveLogic);
+                commandForExecution.UnExecute();
+                this.currentCommandIndex--;
+            }
+        }
+
+        public void Compute()
+        {
+            CommandExecutor commandForExecution = new CommandExecutor(command, moveLogic);
+            commandForExecution.Execute();
+
+            this.commands.Add(command);
+            this.currentCommandIndex++;
+        }
+    }
+}
---
+++ Source/Labyrinth.Logic/Commands/CommandExecutor.cs
@@ -0,0 +1,47 @@
+﻿namespace Labyrinth.Logic.Commands
+{
+    using System;
+    using Labyrinth.Logic.Contracts;
+    using Labyrinth.Models.Contracts;
+    using Models.Players;
+    using Models;
+
+    public class CommandExecutor : ICommandExecutor
+    {
+        private string command;
+        private MoveLogic moveLogic;
+
+        public CommandExecutor(string command, MoveLogic moveLogic)
+        {
+            this.command = command;
+            this.moveLogic = moveLogic;
+        }
+
+        public void Execute()
+        {
+            this.moveLogic.MakeMove(command);
+        }
+
+        public void UnExecute()
+        {
+            this.moveLogic.MakeMove(Undo(command));
+        }
+
+        public string Undo(string command)
+        {
+            switch (command)
+            {
+                case "d":
+                    return "u";
+                case "u":
+                    return "d";
+                case "l":
+                    return "r";
+                case "r":
+                    return "l";
+                default:
+                    throw new ArgumentException("wrong command");
+            }
+        }
+    }
+}
