574798a36923799307d8e47665d8531d3b80504c
DJBuro <DJBuro@gmail.com>
2015-10-10 20:46:45 +0300

    Added new test for console operations

---
+++ tests/LabirynthTests/Enginetest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labyrinth.Console;
+using Moq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LabirynthTests
+{
+    [TestClass]
+    public class Enginetest
+    {
+        [TestInitialize]
+
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            var outLines = new List<string>();
+            var mockWriter = new Mock<TextWriter>();
+            mockWriter.Setup(writer => writer.WriteLine(It.IsAny<string>()))
+                .Callback<string>(s => outLines.Add(s));
+            Console.SetOut(mockWriter.Object);
+
+            var logger = new Renderer();
+            logger.RenderMessage("foo");
+
+            Assert.AreEqual(1, outLines.Count);
+            Assert.AreEqual("foo", outLines[0]);
+            
+        }
+    }
+}
---
+++ tests/LabirynthTests/TextWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LabirynthTests
+{
+    public class MyTextWriter : TextWriter
+    {
+        public List<string> messages = new List<string>();
+
+        public override Encoding Encoding
+        {
+            get
+            {
+                return Encoding.UTF8;
+            }
+        }
+
+        public override void WriteLine(string msg)
+        {
+            messages[0] = msg;
+            this.Return();
+        }
+
+        public string Return()
+        {
+            return messages[0];
+        }
+    }
+}
