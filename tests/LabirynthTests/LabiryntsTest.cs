c5a37abfed0f01f433519487f6d365b6808a6289
DJBuro <DJBuro@gmail.com>
2015-10-10 12:38:00 +0300

    Added Test for Board

---
+++ tests/LabirynthTests/LabiryntsTest.cs
@@ -0,0 +1,40 @@
+﻿namespace LabirynthTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Labyrinth.Models;
+    using Labyrinth.Models.Symbols;
+    using Labyrinth.Common.Enum;
+
+    [TestClass]
+    public class LabiryntsTest
+    {
+        [TestMethod]
+        public void IsBoardInstaceIsSingle()
+        {
+            var board = Board.Instance;
+            var board2 = Board.Instance;
+            Assert.AreSame(board, board2);
+        }
+
+        [TestMethod]
+        public void IsBoardAreSameSymbolReturnsRight()
+        {
+            var board = Board.Instance;
+            Symbol symbol = new EmptySpaceSymbol();
+            board.Field.SetValue(symbol, 2, 2);
+            bool result = board.AreSymbolsEqual(2, 2, SymbolFactory.GetSymbol(SymbolsEnum.EmptySpace));
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void IsBoardReplaceSymbols()
+        {
+            var board = Board.Instance;
+            Symbol symbolToReplace = new EmptySpaceSymbol();
+            Symbol newSymbol = new FilledSpaceSymbol();
+            board.Field.SetValue(symbolToReplace, 2, 2);
+            board.ReplaceSymbol(2, 2, newSymbol);
+            Assert.AreEqual(board.Field.GetValue(2, 2), newSymbol);
+        }
+    }
+}
