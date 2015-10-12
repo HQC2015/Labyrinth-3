8ddae32c4b03beee8b69d60a015836e9abd2fd45
DJBuro <DJBuro@gmail.com>
2015-10-12 15:03:14 +0300

    Last test!

---
+++ tests/LabirynthTests/CommandControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Labyrinth.Logic.Commands;
+using Labyrinth.Models.Players;
+
+namespace LabirynthTests
+{
+    [TestClass]
+    public class CommandControllerTests
+    {
+        //[TestMethod]
+        //public void IsCommandRecieverInvokeRightPrivateMethods()
+        //{
+        //    var mockReciever = new Mock<CommandReceiver>();
+        //    mockReciever.Setup(x => x.SetSuccessor(It.IsAny<CommandExecutor>())).Verifiable();
+        //    mockReciever.Verify(x => x.SetSuccessor(It.IsAny<CommandExecutor>()), Times.Once);
+        //
+        //}
+
+        //[TestMethod]
+        //public void IsProcessCommandInvokeRightMethod()
+        //{
+        //    var player = new Player();
+        //    var cc = new Mock<CommandController>(player);
+        //    cc.Object.ProcessCommand("d");
+        //    
+        //
+        //}
+    }
+}
